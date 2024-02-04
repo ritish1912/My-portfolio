c959835e521b22ce04bee5eef1b3aec1d99a3f91
ritish1912 <ritish1912@gmail.com>
2024-02-04 20:16:03 +0530

    Home-section-complete

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import styles from "../styles/homePage.module.css";
 
 function HomePage() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.poster}>
+        <div className={styles.name}>HEY, I'M RITISH SRIVASTAVA</div>
+        <div className={styles.info}>
+          A Frontend focused Web Developer building the Frontend of Websites and
+          Web Applications that leads to the success of the overall product
+        </div>
+        <a className={styles.button}>Projects</a>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
