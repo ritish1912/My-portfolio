89227e580a3c43de2b71e0e760fede206d7415cd
ritish1912 <ritish1912@gmail.com>
2024-02-06 02:11:03 +0530

    social-links-updated

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,8 +7,9 @@ function HomePage() {
       <div className={styles.poster}>
         <div className={styles.name}>HEY, I'M RITISH SRIVASTAVA</div>
         <div className={styles.info}>
-          A Frontend focused Web Developer building the Frontend of Websites and
-          Web Applications that leads to the success of the overall product
+          A Backend focused Web Developer building both the frontend and backend
+          of Websites and Web Applications that lead to the success of the
+          overall product
         </div>
         <a className={styles.button}>Projects</a>
       </div>
