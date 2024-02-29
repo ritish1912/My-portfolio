1949ed0f9a460e16bfe12959e3c87eec3de26a00
ritish1912 <ritish1912@gmail.com>
2024-03-01 03:33:59 +0530

    project-sectionin-progress-laptop-image-blocker-fixed

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "../styles/projectCard.module.css";
 
-function ProjectCard() {
-  return <div></div>;
+function ProjectCard({ laptopImg, description, isDeployed }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.laptop_img}>
+        <img src={laptopImg} alt="" />
+      </div>
+      <div className={styles.overview}>
+        <div className={styles.p_name}></div>
+        <div className={styles.description}></div>
+        <div className={styles.buttons}>
+          <div className={styles.button}>Live</div>
+          <div className={styles.button}>Github</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../styles/projects.module.css";
+import ProjectCard from "./ProjectCard";
+import portfolioImg from "../resources/Portfolio_laptop.png";
 
 function Projects() {
   return (
@@ -11,6 +13,34 @@ function Projects() {
           each of them I have provided their Github link for reference
         </div>
       </div>
+      <ProjectCard
+        laptopImg={portfolioImg}
+        description={
+          "Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally"
+        }
+        isDeployed={true}
+      />
+      <ProjectCard
+        laptopImg={portfolioImg}
+        description={
+          "Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally"
+        }
+        isDeployed={true}
+      />
+      <ProjectCard
+        laptopImg={portfolioImg}
+        description={
+          "Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally"
+        }
+        isDeployed={true}
+      />
+      <ProjectCard
+        laptopImg={portfolioImg}
+        description={
+          "Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally"
+        }
+        isDeployed={true}
+      />
     </div>
   );
 }
